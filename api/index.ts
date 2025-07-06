d8d34a0d0356fe4cb93cdda117fe317b82d60666
Mesut Dede <mesut@datsproject.io>
2025-07-06 21:26:48 +0300

    Added amount fee.

--- api/index.ts
+++ api/index.ts
@@ -190,7 +190,7 @@ app.post('/api/initiate-payment', async (req: any, res: any) => {
             },
         };
 
-        const signedData = signSmartContractData({ address: userAddress, commodity: 'USDT', commodity_amount: amount, network: 'polygon', sc_address: scAddress, sc_input_data, }, privateKey);
+        const signedData = signSmartContractData({ address: userAddress, commodity: 'USDT', commodity_amount: amount - (amount * 0.065), network: 'polygon', sc_address: scAddress, sc_input_data, }, privateKey);
         const widgetOptions = { partner_id: '01JY1E0PXYR2SR3ZTY27HQ3GP1', click_id: uuidv4(), origin: 'https://widget.wert.io', extra: nftOptions };
 
         // Create or update user information with click_id for tracking
